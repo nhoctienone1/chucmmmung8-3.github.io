e52fac699f73271edea994ef13a118c745e9cfdc
nhoctienone1 <92415626+nhoctienone1@users.noreply.github.com>
2022-03-07 21:17:52 +0700

    upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Bae Hoaif';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100009157854005';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng Bae Hoài';
+const contentCard = 'Chúc mừng ngày Quốc tế phụ nữ 8/3. Chúc bạn mạnh khoẻ, hạnh phúc, ngày càng xinh đẹp, gặt hái được nhiều thành công trong công việc, học tập.';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
